01796f1ff3146dab29a12db7f9ddc0856fe2d1b8
Vansh Negi <negivansh88@gmail.com>
2026-02-28 00:54:11 +0530

    fix next.js server component errors by adding use client directives

--- frontend/app/profile/[username]/page.tsx
+++ frontend/app/profile/[username]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import PostItem, { PostType } from "../../../components/PostItem";
--- frontend/components/PostItem.tsx
+++ frontend/components/PostItem.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { Heart, MessageCircle, Share2, MoreHorizontal } from 'lucide-react';
